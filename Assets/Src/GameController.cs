acdfe4bb224332a0698a91e8663d482b1d6f554d
px <413323644@qq.com>
2021-03-03 11:33:48 +0800

    ...

---
+++ Assets/Src/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private BulletController bullet_controller;
+    private PlayerController player_controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bullet_controller = GameObject.FindObjectOfType<BulletController>();
+        player_controller = GameObject.FindObjectOfType<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
